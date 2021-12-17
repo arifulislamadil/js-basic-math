467e45d8e97907e8803a86635be0e74a288af813
Arifu Islam Adil <adil.islam.arif@gmail.com>
2021-12-17 09:32:41 +0600

    js math random name

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,15 @@
-function fibonacciSeries(number) {
+function fibonacciSeries(num) {
+    if (typeof num != "number") {
+        return "Please input a valid number"
+    }
+    if (num < 2) {
+        return "Please input a positive number more then 1"
+    }
     let fibo = [0, 1]
-    for (var i = 2; i <= number; i++) {
+    for (var i = 2; i <= num; i++) {
         fibo[i] = fibo[i - 1] + fibo[i - 2];
     }
     return fibo;
 }
-console.log(fibonacciSeries(10))
+console.log(fibonacciSeries(-7))
 //Output: [ 0, 1,  1,  2,  3,5, 8, 13, 21, 34, 55]
---
+++ javascript-math.js
@@ -0,0 +1,12 @@
+const random = Math.random();
+const randomMultiple = random * 10;
+console.log(randomMultiple)
+const randomFloor = Math.floor(randomMultiple);
+console.log(randomFloor);
+const randomCeil = Math.ceil(randomMultiple);
+console.log(randomCeil)
+const randomRound = Math.round(randomMultiple)
+console.log(randomRound)
+
+
+
